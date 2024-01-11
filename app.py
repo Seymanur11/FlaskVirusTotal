fe19342729c85b08af3adceb34ee97fad7af6b9a
Seymanur11 <119653340+Seymanur11@users.noreply.github.com>
2024-01-11 16:42:14 +0300

    some changes

--- app.py
+++ app.py
@@ -5,13 +5,7 @@ app = Flask(__name__)
 
 API_KEY = 'd095ca647af955e6df735431b2bb9f39374fa8e3e7e5c86e0892b6ff6042702c'
 
-def URLSearch(params, headers):
-    response = requests.get('https://www.virustotal.com/vtapi/v2/url/report',
-                            params=params, headers=headers)
-    json_response = {}
-    if response.ok:
-        json_response = response.json()
-        return json_response
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,7 +23,13 @@ def url_search():
     return render_template('index.html')
 
 
-
+def URLSearch(params, headers):
+    response = requests.get('https://www.virustotal.com/vtapi/v2/url/report',
+                            params=params, headers=headers)
+    json_response = {}
+    if response.ok:
+        json_response = response.json()
+        return json_response
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
